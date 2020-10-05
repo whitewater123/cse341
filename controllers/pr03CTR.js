148adee3fae897d94ccb06d5d3c977ce33a4941e
Bluelight413 <was16006@byui.edu>
2020-10-04 20:08:32 -0600

    Program is finally running

--- controllers/pr03CTR.js
+++ controllers/pr03CTR.js
@@ -2,11 +2,14 @@ const Product = require('../models/pr03MDL');
 
 exports.getProducts = (req, res, next) => {
     Product.fetchAll(products => {
+        /*let retrievedData = global.jsonResponse.filter(x => x.name.toLowerCase().includes(searchedValue.toLowerCase()))
+    console.log(retrievedData)*/
         res.render('pages/pr03VIW', { 
             title: 'Prove 03', 
             path: '/pr03', // For pug, EJS 
             activeTA03: true, // For HBS
             contentCSS: true, // For HBS
+            data: products,
         });
     });
 };
--- routes/proveRoutes/pr03RT.js
+++ routes/proveRoutes/pr03RT.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const pr03Controller = require('../../controllers/pr03CTR.js');
 
-router.get('/pr03', pr03Controller.getProducts);
-router.post('/',(req, res, next) => {
+router.get('/', pr03Controller.getProducts);
+/*router.post('/',(req, res, next) => {
     let retrievedData = global.jsonResponse.filter(x => x.name.toLowerCase().includes(searchedValue.toLowerCase()))
     console.log(retrievedData)
     res.render('pages/pr03VIW', {
@@ -12,18 +12,6 @@ router.post('/',(req, res, next) => {
         path: '/pr03'
 });
 return res.end();
-});
-/*var jsonEngine = require('../../controllers/pr03CTR.js');
-
-router.get('/', jsonEngine.processJson)
-      .post('/',(req, res, next) => {
-          let retrievedData = global.jsonResponse.filter(x => x.name.toLowerCase().includes(searchedValue.toLowerCase()))
-          console.log(retrievedData)
-          res.render('pages/pr03VIW', {
-              name: 'JSON Search',
-              data: retrievedData,
-              path: '/pr03'
-    });
-    return res.end();
 });*/
+
 module.exports = router;
